29ec56bdb4435ffd1214c0a6efbdb98d46e4127c
Maureen E <maureenekeugo1@gmail.com>
2025-05-21 15:28:32 -0400

    Flatten Binary Tree to Linked List Solution

---
+++ Binary Tree General/Problem8.js
@@ -0,0 +1,56 @@
+//  Flatten Binary Tree to Linked List
+// Medium
+// Topics
+// Companies
+// Hint
+// Given the root of a binary tree, flatten the tree into a "linked list":
+
+// The "linked list" should use the same TreeNode class where the right child pointer points to the next node in the list and the left child pointer is always null.
+// The "linked list" should be in the same order as a pre-order traversal of the binary tree.
+ 
+
+// Example 1:
+
+
+// Input: root = [1,2,5,3,4,null,6]
+// Output: [1,null,2,null,3,null,4,null,5,null,6]
+// Example 2:
+
+// Input: root = []
+// Output: []
+// Example 3:
+
+// Input: root = [0]
+// Output: [0]
+ 
+
+// SOLUTION
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {void} Do not return anything, modify root in-place instead.
+ */
+var flatten = function(root) {
+    //check if the root is null return null
+    if(!root) return null;
+    root.next= null;
+    //make the left .next  eqaul to root.left 
+    root.next  = flatten(root.left)
+
+//recursively go the left 
+    root.next = flatten(root.right)
+//return root
+root
+
+//
+
+    
+};
